f18eda219f8a9a68bb85e198aa71af82ad591774
Adekabang <iban.ninja@gmail.com>
2024-09-29 01:05:19 -0400

    fix https:

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -2,11 +2,11 @@ import PocketBase from 'pocketbase';
 
 import { writable } from 'svelte/store';
 
-export const pb = new PocketBase('http://pbc.103.177.95.246.sslip.io/');
+export const pb = new PocketBase('https://pbc.ksara.xyz/');
 
 export const currentUser = writable(pb.authStore.model);
 
 pb.authStore.onChange((auth) => {
-  console.log('authStore changed', auth);
+  // console.log('authStore changed', auth);
   currentUser.set(pb.authStore.model);
 })
